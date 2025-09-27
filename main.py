1a55f64898a048c05a10bafbfbb769e32a010fc6
Harsh Kurware <harshkurware03@gmail.com>
2025-09-28 01:14:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 from app import create_app
 
@@ -8,5 +7,4 @@ if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     host = os.environ.get('HOST', '0.0.0.0')
     debug = os.environ.get('FLASK_DEBUG', 'True').lower() == 'true'
-    
     app.run(host=host, port=port, debug=debug)
